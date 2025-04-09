e2fcc8c0e4130756b9927784c1d485e493b4ff51
Sithara Liyanage <dev.sitharaliyanage@gmail.com>
2025-04-09 10:03:08 +0530

    update a bit

--- src/main/java/lk/ijse/simplechat2/ServerFormController.java
+++ src/main/java/lk/ijse/simplechat2/ServerFormController.java
@@ -5,12 +5,31 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
 
     public TextArea serverResents;
     public TextField serverTxt;
     public Button sSendBtn;
 
+    public void initialize() {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(4009);
+            Socket socket = serverSocket.accept();
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String message = dataInputStream.readUTF();
+            serverResents.appendText(message+"\n");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     public void serverSendBtnOnAction(ActionEvent actionEvent) {
 
         System.out.println(serverTxt.getText());
